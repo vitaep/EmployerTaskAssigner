1cbf65bf302355a4dc92fe5c9cbf252d6422c483
Vitor <souzavitorb1@gmail.com>
2025-02-20 18:57:43 -0300

    first commit

---
+++ src/main/java/dev/vitorbomfim/EmployersTaskAssigner/EmployersTaskAssignerApplication.java
@@ -0,0 +1,13 @@
+package dev.vitorbomfim.EmployersTaskAssigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployersTaskAssignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployersTaskAssignerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vitorbomfim/EmployersTaskAssigner/EmployersTaskAssignerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vitorbomfim.EmployersTaskAssigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployersTaskAssignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
