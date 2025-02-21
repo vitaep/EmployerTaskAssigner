97e1ae3263f25439a98c09c3c1f136648b1c4cb0
Vitor <souzavitorb1@gmail.com>
2025-02-21 04:20:23 -0300

    fix(database): removed old database files and trying connect database with entity tables

--- src/main/java/dev/vitorbomfim/EmployersTaskAssigner/EmployersTaskAssignerApplication.java
+++ src/main/java/dev/vitorbomfim/EmployersTaskAssigner/EmployersTaskAssignerApplication.java
@@ -2,8 +2,10 @@ package dev.vitorbomfim.EmployersTaskAssigner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "dev.vitorbomfim.EmployersTaskAssigner")
 public class EmployersTaskAssignerApplication {
 
 	public static void main(String[] args) {
