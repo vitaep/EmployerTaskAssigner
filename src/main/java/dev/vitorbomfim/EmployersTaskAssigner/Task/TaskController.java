4a7bd3ac16722128dc84fe8d0bce5c2bbddb3751
Vitor Bomfim <souzavitorb1@gmail.com>
2025-02-23 17:12:56 -0300

    feat: update task controller with data mock

--- src/main/java/dev/vitorbomfim/EmployersTaskAssigner/Task/TaskController.java
+++ src/main/java/dev/vitorbomfim/EmployersTaskAssigner/Task/TaskController.java
@@ -1,16 +1,45 @@
 package dev.vitorbomfim.EmployersTaskAssigner.Task;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/task")
 public class TaskController {
 
-    @GetMapping("/taskRegister")
-    public String helloWorld() {
-        return "Hello World";
+    // Add task (CREATE)
+
+    @PostMapping("/add")
+    public String addTask(){
+        return "Task added";
     }
+
+    // Get all tasks (READ)
+
+    @GetMapping("/get")
+    public String getTask(){
+        return "Tasks";
+    }
+
+    // Search tasks by id (READ)
+
+    @GetMapping("/getId")
+    public String getTaskById(){
+        return "Task by id";
+    }
+
+    // Update task data (UPDATE)
+
+    @PutMapping("/update")
+    public String updateTask(){
+        return "Task updated";
+    }
+
+    // Delete task (DELETE)
+
+    @DeleteMapping("/deleteId")
+    public String deleteTaskById(){
+        return "Task deleted by id";
+    }
+
 
 }
