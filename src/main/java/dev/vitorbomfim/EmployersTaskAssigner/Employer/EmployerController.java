cc4d0de1f3e31285be0e9b125b03b7a890324d59
Vitor Bomfim <souzavitorb1@gmail.com>
2025-02-23 17:03:27 -0300

    feat: update the controller with data mock

--- src/main/java/dev/vitorbomfim/EmployersTaskAssigner/Employer/EmployerController.java
+++ src/main/java/dev/vitorbomfim/EmployersTaskAssigner/Employer/EmployerController.java
@@ -1,8 +1,6 @@
 package dev.vitorbomfim.EmployersTaskAssigner.Employer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,41 @@ public class EmployerController {
     @GetMapping("/employeeRegister")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    // Add employer (CREATE)
+
+    @PostMapping("/add")
+    public String addEmployer(){
+        return "Employer added";
+    }
+
+    // Get all employers (READ)
+
+    @GetMapping("/get")
+    public String getEmployer(){
+        return "Employers";
+    }
+
+    // Search employer by id (READ)
+
+    @GetMapping("/getId")
+    public String getEmployerById(){
+        return "Employers by id";
+    }
+
+    // Update employer data (UPDATE)
+
+    @PutMapping("/updateEmployer")
+    public String updateEmployer(){
+        return "Employer updated";
+    }
+
+    // Delete employer (DELETE)
+
+    @DeleteMapping("/deleteId")
+    public String deleteEmployerById(){
+        return "Employer deleted by id";
     }
 
 }
